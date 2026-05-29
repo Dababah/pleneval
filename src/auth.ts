160cf1c1569bd9b50d333ed2cf2c9049b1f79e57
Your Name <your.email@example.com>
2026-05-29 18:21:05 +0700

    opop

--- src/auth.ts
+++ src/auth.ts
@@ -5,7 +5,7 @@ import authConfig from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { 
+  session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
@@ -15,14 +15,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         token.sub = user.id;
         token.username = (user as any).username;
       }
-      
+
       // Handle session update on the client
       if (trigger === "update" && session) {
         if (session.name) token.name = session.name;
         if (session.image) token.image = session.image;
         if (session.username) token.username = session.username;
       }
-      
+
       return token;
     },
     async session({ session, token }) {
@@ -30,7 +30,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         session.user.id = token.sub;
         (session.user as any).username = token.username;
         if (token.name) session.user.name = token.name;
-        if (token.image) session.user.image = token.image;
+
+        // SUNTIKKAN 'as string' DI SINI UNTUK MENJINAKKAN TYPESCRIPT
+        if (token.image) session.user.image = token.image as string;
       }
       return session;
     },
